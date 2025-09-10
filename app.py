5f88bea454e2b687ed1c6c2911d4ae5e7fc29c59
Guilherme Gonçalves <ggdossantos2402@gmail.com>
2025-09-10 10:07:08 -0300

    feat: inicial app Flask Hello World

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Cria a aplicação Flask
+app = Flask(__name__)
+
+# Rota principal
+@app.route("/")
+def home():
+    return "Hello, DevOps World!"
+
+# Ponto de entrada para rodar localmente
+if __name__ == "__main__":
+    # host 0.0.0.0 permite acesso externo (útil no container/VM)
+    app.run(host="0.0.0.0", port=5000, debug=True)
